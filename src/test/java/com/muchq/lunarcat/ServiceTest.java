8b67ea6533ff1531222df5be54b070d8a3a08b01
Andy Aylward <aaylward@gmail.com>
2017-02-26 16:09:10 -0500

    only one subclass

--- src/test/java/com/muchq/lunarcat/ServiceTest.java
+++ src/test/java/com/muchq/lunarcat/ServiceTest.java
@@ -1,14 +1,39 @@
 package com.muchq.lunarcat;
 
+import com.hubspot.horizon.HttpClient;
 import com.hubspot.horizon.HttpRequest;
 import com.hubspot.horizon.HttpResponse;
+import com.hubspot.horizon.apache.ApacheHttpClient;
+import com.muchq.lunarcat.config.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.net.ServerSocket;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ServiceTest extends ServiceTestBase {
+public class ServiceTest {
+  private static Service service;
+  private final static HttpClient client = new ApacheHttpClient();
+
+  private static Integer port;
+
+  @BeforeClass
+  public static void setup() {
+    Configuration configuration = Configuration.newBuilder()
+        .withPort(getPort())
+        .withBasePackage(Package.getPackage("com.muchq.lunarcat"))
+        .build();
+    service = new Service(configuration);
+    service.runNoWait();
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    service.shutDown();
+  }
 
   @Test
   public void itServesRequests() {
@@ -74,5 +99,27 @@ public class ServiceTest extends ServiceTestBase {
         .build();
     HttpResponse response = getClient().execute(request);
     assertThat(response.getStatusCode()).isEqualTo(400);
+  }
+
+  public static HttpClient getClient() {
+    return client;
+  }
+
+  public static int getPort() {
+    if (port == null) {
+      port = getAvailablePort();
+    }
+    return port;
+  }
+
+  private static int getAvailablePort() {
+    try {
+      ServerSocket serverSocket = new ServerSocket(0);
+      int port = serverSocket.getLocalPort();
+      serverSocket.close();
+      return port;
+    } catch (Exception e) {
+      throw new RuntimeException();
+    }
   }
 }
