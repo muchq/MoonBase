f59c2df4f62f64a0875602889ec41c884be1085c
Andy Aylward <andy@eggsample.net>
2023-06-04 19:17:48 -0400

    fixes #33 add gtest example :test_tube:
     - also do a bunch of random stuff

---
+++ src/main/cpp/example_cc_go/example_cc_go.h
@@ -0,0 +1,10 @@
+#ifndef EXAMPLE_CC_GO_H // include guard
+#define EXAMPLE_CC_GO_H
+
+#include <string>
+
+namespace example_cc_go {
+    std::string MakeGreeting(std::string name);
+}
+
+#endif
--- src/main/cpp/example_service/example_service.cc
+++ src/main/cpp/example_service/example_service.cc
@@ -9,7 +9,6 @@
 #include "absl/strings/str_format.h"
 
 #include "src/main/protos/exampleservice.grpc.pb.h"
-#include "src/main/golang/example_lib/example_lib.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -22,9 +21,8 @@ using exampleservice::HelloRequest;
 class GreeterServiceImpl final : public Greeter::Service {
   Status SayHello(ServerContext* context, const HelloRequest* request,
                   HelloReply* reply) override {
-    // this is awful, eh?
-    std::string greeting(Greet(const_cast<char*>(request->name().c_str())));
-    reply->set_message(greeting);
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
     return Status::OK;
   }
 };
