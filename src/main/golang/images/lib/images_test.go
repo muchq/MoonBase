edd0f71d793ade19c49af687ba6254117ea499b8
Andy Aylward <andy@eggsample.net>
2023-06-09 00:34:05 -0400

    tidy test slightly

--- src/main/golang/images/lib/images_test.go
+++ src/main/golang/images/lib/images_test.go
@@ -2,34 +2,33 @@ package lib
 
 import (
 	"image"
+	"image/color"
 	"testing"
 )
 
 func TestGreyScaleImage(t *testing.T) {
 	img := image.NewRGBA(image.Rectangle{
 		Min: image.Point{X: 0, Y: 0},
-		Max: image.Point{X: 10, Y: 5},
+		Max: image.Point{X: 3, Y: 3},
 	})
-	pixelComponents := img.Pix
-	i := 0
-	for i < len(pixelComponents) {
-		modFour := i % 4
-		if modFour == 0 {
-			pixelComponents[i] = 0 // R
-		} else if modFour == 1 {
-			pixelComponents[i] = 100 // G
-		} else if modFour == 2 {
-			pixelComponents[i] = 50 // B
-		} else {
-			pixelComponents[i] = 255 // A
-		}
-		i++
-	}
+	fillImage(*img, color.RGBA{R: 0, G: 100, B: 50, A: 255})
 
 	grey := GreyScale(img)
 	for _, b := range grey.Pix {
 		if b != 64 {
 			t.Fail()
 		}
+	}
+}
+
+func fillImage(img image.RGBA, rgba color.RGBA) {
+	row := 0
+	for row < img.Bounds().Dy() {
+		col := 0
+		for col < img.Bounds().Dx() {
+			img.SetRGBA(row, col, rgba)
+			col++
+		}
+		row++
 	}
 }
