2f89910975a85c98569fc5a5e7467ddf0af61427
Andy Aylward <andy@eggsample.net>
2023-06-24 17:18:23 -0400

    🧹  Scalafmt (#58)
    * update env setup + add scalafmt

--- jvm/src/main/scala/com/muchq/spitha/SparkApp.scala
+++ jvm/src/main/scala/com/muchq/spitha/SparkApp.scala
@@ -4,8 +4,7 @@ import org.apache.spark.sql.SparkSession
 
 abstract class SparkApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder
+    val spark = SparkSession.builder
       .appName("Spark App")
       .config("spark.master", "spark://upstairs-mini.local:7077")
       .getOrCreate()
--- jvm/src/main/scala/com/muchq/spitha/example/SparkExample.scala
+++ jvm/src/main/scala/com/muchq/spitha/example/SparkExample.scala
@@ -11,12 +11,15 @@ object SparkExample extends SparkApp {
 
   override def run(args: Array[String])(implicit spark: SparkSession): Unit = {
     val slices = if (args.length > 0) args(0).toInt else 2
-    val n = computeSequenceSize(slices)
-    val count = spark.sparkContext.parallelize(seq = 1 until n, slices).map { _ =>
-      val x = random * 2 - 1
-      val y = random * 2 - 1
-      if (x*x + y*y <= 1) 1 else 0
-    }.reduce(_ + _)
+    val n      = computeSequenceSize(slices)
+    val count = spark.sparkContext
+      .parallelize(seq = 1 until n, slices)
+      .map { _ =>
+        val x = random * 2 - 1
+        val y = random * 2 - 1
+        if (x * x + y * y <= 1) 1 else 0
+      }
+      .reduce(_ + _)
     log.info(s"Pi is roughly ${4.0 * count / (n - 1)}")
   }
 
