62860edbd7a070fc5c261c47d76d42af5cdfaf72
Andy Aylward <andy@eggsample.net>
2023-07-23 23:49:18 -0400

    clean up mapper header

--- cpp/golf_service/game_state_mapper.h
+++ cpp/golf_service/game_state_mapper.h
@@ -1,7 +1,6 @@
 #ifndef CPP_GOLF_SERVICE_GAME_STATE_MAPPER_H
 #define CPP_GOLF_SERVICE_GAME_STATE_MAPPER_H
 
-#include <memory>
 #include <string>
 
 #include "cpp/cards/card_mapper.h"
@@ -10,20 +9,14 @@
 
 namespace golf {
 
-typedef std::unique_ptr<golf_ws::GameStateResponse> ResponsePtr;
-
 class GameStateMapper {
  public:
   GameStateMapper(const CardMapper _cm) : cm(_cm) {}
-  std::string gameStateJson(const GameStatePtr& gameStatePtr, const std::string& username) const;
   golf_ws::GameStateResponse gameStateToProto(const GameStatePtr& gameStatePtr,
                                               const std::string& username) const;
 
  private:
   const CardMapper cm;
-  std::string writeString(const std::string& name, const std::string& value);
-  std::string writeInt(const std::string& name, const int value);
-  std::string writeBool(const std::string& name, const bool value);
 };
 }  // namespace golf
 
