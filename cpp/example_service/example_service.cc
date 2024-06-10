48675fc8909bb7c1c96d730fdb817572de57528e
Andy Aylward <andy@eggsample.net>
2024-06-09 23:21:08 -0700

    add example service unit test 🧪  (#162)
    add example service unit test

--- cpp/example_service/example_service.cc
+++ cpp/example_service/example_service.cc
@@ -1,47 +1,17 @@
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include "cpp/example_service/example_service.h"
+
 #include <grpcpp/grpcpp.h>
-#include <grpcpp/health_check_service_interface.h>
-
-#include <iostream>
-#include <memory>
 #include <string>
-
-#include "absl/strings/str_format.h"
 #include "protos/example_cc_grpc/example_service.grpc.pb.h"
 
-using example_service::Greeter;
 using example_service::HelloReply;
 using example_service::HelloRequest;
-using grpc::Server;
-using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-class GreeterServiceImpl final : public Greeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request, HelloReply* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
+Status GreeterServiceImpl::SayHello(ServerContext* context, const HelloRequest* request,
+                                    HelloReply* reply) {
+  std::string prefix("Hello ");
+  reply->set_message(prefix + request->name());
+  return Status::OK;
 };
-
-void RunServer(uint16_t port) {
-  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-  GreeterServiceImpl service;
-
-  grpc::EnableDefaultHealthCheckService(true);
-  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-
-  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());  // no auth
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-
-  std::cout << "Server listening on " << server_address << std::endl;
-  server->Wait();
-}
-
-int main() {
-  RunServer(8088);
-  return 0;
-}
---
+++ cpp/example_service/main.cc
@@ -0,0 +1,30 @@
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "cpp/example_service/example_service.h"
+#include "protos/example_cc_grpc/example_service.grpc.pb.h"
+
+using grpc::ServerBuilder;
+using grpc::Server;
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  GreeterServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());  // no auth
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main() {
+  RunServer(8089);
+  return 0;
+}
