4ea2bcbc26fedaba1cf614b403497ae2e2e2dc20
Andy Aylward <andy@eggsample.net>
2024-06-30 11:06:01 -0400

    move rust proto generation into protos

--- rust/helloworld_tonic/build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    match env::var("PROTO_PATH") {
-        Ok(p) =>  tonic_build::compile_protos(p)?,
-        Err(e) => println!("Oops: {}", e.to_string())
-    };
-    Ok(())
-}
