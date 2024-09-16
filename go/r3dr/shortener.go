3d4fbcab0639b6d9158d98e95b3f41efad2aa667
Andy Aylward <andy@eggsample.net>
2024-09-15 22:31:45 -0400

    validate ShortenRequest

--- go/r3dr/shortener.go
+++ go/r3dr/shortener.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
 type Shortener struct {
 	urlDao UrlDao
 }
@@ -9,6 +15,16 @@ func NewShortener(urlDao UrlDao) *Shortener {
 }
 
 func (s *Shortener) Shorten(request ShortenRequest) (ShortenResponse, error) {
+	if !(strings.HasPrefix(request.LongUrl, "http://") || strings.HasPrefix(request.LongUrl, "https://")) {
+		return ShortenResponse{}, errors.New("longUrl must include protocol")
+	}
+	if len(request.LongUrl) < 11 {
+		// shortest allowed url is like http://g.co
+		return ShortenResponse{}, errors.New("longUrl too short")
+	}
+	if request.ExpiresAt != 0 && request.ExpiresAt < time.Now().UnixMilli() {
+		return ShortenResponse{}, errors.New("expiresAt is less then now")
+	}
 	slug, err := s.urlDao.InsertUrl(request.LongUrl, request.ExpiresAt)
 	return ShortenResponse{Slug: slug}, err
 }
