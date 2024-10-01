a6f3639d148789c1382f710451686f4efa95e560
Andy Aylward <andy@eggsample.net>
2024-10-01 14:21:08 -0400

    format-cc

--- cpp/example_service/main.cc
+++ cpp/example_service/main.cc
@@ -1,6 +1,7 @@
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
+
 #include <cstdlib>
 
 #include "cpp/example_service/example_service.h"
