dff1146b4061207f7cc79acebd3c51ac9cd1818a
Andy Aylward <andy@eggsample.net>
2024-10-01 14:26:20 -0400

    topg

--- go/r3dr/sql/snapshot_001.sql
+++ go/r3dr/sql/snapshot_001.sql
@@ -1,12 +1,11 @@
-CREATE TABLE `urls` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `short_url` varchar(20) NOT NULL,
-    `long_url` varchar(1000) NOT NULL,
-    `created_at` timestamp NULL DEFAULT current_timestamp(),
-    `expires_at` timestamp DEFAULT NULL,
-    `created_by` varchar(40) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `slug_idx` (`short_url`) USING HASH
+-- pgsql
+CREATE SEQUENCE url_ids;
+
+CREATE TABLE urls (
+    id BIGINT PRIMARY KEY,
+    short_url VARCHAR(20) NOT NULL UNIQUE,
+    long_url VARCHAR(1000) NOT NULL,
+    created_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP DEFAULT NULL,
+    created_by VARCHAR(40) DEFAULT NULL
 )
-  CHARACTER SET 'utf8mb4'
-  COLLATE 'utf8mb4_bin';
