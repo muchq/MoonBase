98636526348caab05fb380090fa467b4a562d402
Andy Aylward <andy@eggsample.net>
2024-10-20 13:47:49 -0400

    push TestClock

--- go/resilience4g/rate_limit/token_bucket_rate_limiter_test.go
+++ go/resilience4g/rate_limit/token_bucket_rate_limiter_test.go
@@ -3,18 +3,18 @@ package rate_limit
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 var factory TokenBucketRateLimiterFactory = TokenBucketRateLimiterFactory{}
+var validConfig RateLimiterConfig = &DefaultRateLimitConfig{
+	maxTokens:  1,
+	refillRate: 1,
+	opCost:     1,
+}
 
 func TestFactoryAllowsValidConfig(t *testing.T) {
-	config := &DefaultRateLimitConfig{
-		maxTokens:  1,
-		refillRate: 1,
-		opCost:     1,
-	}
-
-	_, err := factory.NewRateLimiter(config)
+	_, err := factory.NewRateLimiter(validConfig)
 	assert.Nil(t, err, "err should be nil for valid config")
 }
 
@@ -49,4 +49,22 @@ func TestFactoryValidatesOpCost(t *testing.T) {
 
 	_, err := factory.NewRateLimiter(config)
 	assert.EqualError(t, err, "op cost must be positive")
+}
+
+type TestClock struct {
+	unixSeconds int64
+}
+
+// Now implements the Clock interface
+func (c *TestClock) Now() time.Time {
+	return time.Unix(c.unixSeconds, 0)
+}
+
+func (c *TestClock) Tick() {
+	c.unixSeconds += 1
+}
+
+func TestLimiterAllowsRequestsIfCurrentTokensLessThanOpCost(t *testing.T) {
+	testClock := &TestClock{}
+
 }
