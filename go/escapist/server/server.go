12b127351134d33e74209ba5533702c66491a7eb
Andy Aylward <andy@eggsample.net>
2024-10-31 10:52:47 -0400

    start goscapist

---
+++ go/escapist/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"context"
+	pb "github.com/muchq/moonbase/protos/escapist"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type EscapistServer struct {
+	pb.UnimplementedEscapistServer
+}
+
+func (*EscapistServer) InsertDoc(context.Context, *pb.InsertDocRequest) (*pb.InsertDocResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method InsertDoc not implemented")
+}
+func (*EscapistServer) UpdateDoc(context.Context, *pb.UpdateDocRequest) (*pb.UpdateDocResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateDoc not implemented")
+}
+func (*EscapistServer) FindDocById(context.Context, *pb.FindDocByIdRequest) (*pb.FindDocByIdResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindDocById not implemented")
+}
+func (*EscapistServer) FindDoc(context.Context, *pb.FindDocRequest) (*pb.FindDocResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindDoc not implemented")
+}
+
+func NewEscapistServer() *grpc.Server {
+	escapistServer := &EscapistServer{}
+	s := grpc.NewServer()
+	pb.RegisterEscapistServer(s, escapistServer)
+	return s
+}
