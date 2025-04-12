4b3da295a58f17ac6e3586892350186348c465fc
Andy Aylward <andy@eggsample.net>
2025-04-12 16:32:41 -0400

    add log + start to tidy + prepare to read graph data

--- rust/wordchains/src/main.rs
+++ rust/wordchains/src/main.rs
@@ -1,10 +1,14 @@
+use log::{info, warn, LevelFilter};
+use std::fs::File;
+use std::io::Write;
 use std::{
     collections::{HashMap, VecDeque},
     env,
     fs::read_to_string,
 };
-use std::fs::File;
-use std::io::Write;
+use std::collections::HashSet;
+
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 
 #[derive(Debug, Clone)]
 struct Node {
@@ -12,9 +16,14 @@ struct Node {
     parent: Option<Box<Node>>,
 }
 
+// TODO: handle missing file
 fn read_words(filename: String) -> Vec<String> {
-    read_to_string(filename).unwrap().lines()
+    read_to_string(filename)
+        .unwrap()
+        .lines()
         .map(|l| l.trim().to_lowercase())
+        .collect::<HashSet<String>>()
+        .into_iter()
         .collect()
 }
 
@@ -35,7 +44,7 @@ fn to_path(end: Node) -> Vec<String> {
     let mut path: Vec<String> = vec![end.value.clone()];
     let mut node = end;
     while node.parent.is_some() {
-        let parent = node.parent?;
+        let parent = node.parent.unwrap();
         path.push(parent.value.clone());
         node = *parent;
     }
@@ -88,54 +97,23 @@ fn bfs_for_target(
     None
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        println!("Usage: {} <start> <end>", args[0]);
-        return;
-    }
-
-    let start = &args[1];
-    let end = &args[2];
-
-    if start.len() != end.len() {
-        println!("No chain between {} and {}", start, end);
-        return;
-    }
-
-    // TODO: dictionary path from args
-    let path = "/usr/share/dict/words";
-    //let path = "/Volumes/Envoy Ultra 4TB/words/oed_2/oed_words.txt";
-
-    // TODO: prebuilt dictionary + graph?
-    println!("Reading dictionary from \"{}\"...", path);
-    let words: Vec<String> = read_words(String::from(path))
+fn read_dictionary(path: &str) -> Vec<String> {
+    info!("Reading dictionary from \"{}\"...", path);
+    read_words(String::from(path))
         .iter()
         .filter(|word| word.len() < 9)
+        .filter(|word| word.len() == 5)
         .map(|word| word.to_owned())
-        .collect();
-    let num_words = words.len();
-    let mut word_to_index: HashMap<String, usize> = HashMap::new();
-    for (i, word) in words.iter().enumerate() {
-        word_to_index.insert(word.to_owned(), i);
-    }
+        .collect()
+}
 
-    if !word_to_index.contains_key(start) {
-        println!("{} is not in my dictionary.", &start);
-        return;
-    }
-
-    if !word_to_index.contains_key(end) {
-        println!("{} is not in my dictionary.", &end);
-        return;
-    }
-
-    println!("building graph...");
-    let mut indexes: Vec<u8> = Vec::new();
+fn build_graph(words: &Vec<String>) -> (Vec<Vec<usize>>, Vec<usize>) {
+    info!("building graph...");
     let mut matches: Vec<usize> = Vec::new();
+    let num_words = words.len();
     let mut word_graph: Vec<Vec<usize>> = vec![vec![]; num_words];
     for (i, word1) in words.iter().enumerate() {
-        for j in (i+1)..num_words {
+        for j in (i + 1)..num_words {
             let word2 = words[j].clone();
             if words_are_one_away(word1, &word2) {
                 word_graph[i].push(j);
@@ -145,21 +123,76 @@ fn main() {
             }
         }
     }
-    // println!("{:?}", word_graph);
+    (word_graph, matches)
+}
+
+fn write_graph_to_file(matches: Vec<usize>, path: &str) -> () {
+    let mut indexes: Vec<u8> = Vec::new();
     for m in matches {
         for x in m.to_be_bytes() {
             indexes.push(x);
         }
     }
 
-    let mut file = File::create("60df739928ea654af9a0e7cf8fa19e3f.graph").unwrap();
+    let mut file = File::create(path).unwrap();
     file.write_all(&indexes).unwrap();
-    println!("starting search...");
+}
+
+fn main() {
+    TermLogger::init(
+        LevelFilter::Debug,
+        Config::default(),
+        TerminalMode::Stdout,
+        ColorChoice::Always
+    ).unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        info!("Usage: {} <start> <end>", args[0]);
+        return;
+    }
+
+    let start = &args[1];
+    let end = &args[2];
+
+    if start.len() != end.len() {
+        info!("No chain between {} and {}", start, end);
+        return;
+    }
+
+    // TODO: dictionary path from args
+    let path = "/usr/share/dict/words";
+    //let path = "/Volumes/Envoy Ultra 4TB/words/oed_2/oed_words.txt";
+
+    // TODO: prebuilt dictionary + graph?
+    let words: Vec<String> = read_dictionary(path);
+    let mut word_to_index: HashMap<String, usize> = HashMap::new();
+    for (i, word) in words.iter().enumerate() {
+        word_to_index.insert(word.to_owned(), i);
+    }
+
+    if !word_to_index.contains_key(start) {
+        warn!("{} is not in my dictionary.", &start);
+        return;
+    }
+
+    if !word_to_index.contains_key(end) {
+        warn!("{} is not in my dictionary.", &end);
+        return;
+    }
+
+    let (word_graph, matches) = build_graph(&words);
+
+    // println!("{:?}", word_graph);
+    // TODO: use digest of read_dictionary
+    write_graph_to_file(matches, "60df739928ea654af9a0e7cf8fa19e3f.graph");
+
+    info!("starting search...");
     let target = bfs_for_target(start.clone(), end, &word_graph, &word_to_index, &words);
 
     match target {
-        Some(path) => println!("Path from {} to {}: {:?}", start, end, path),
-        None => println!("No path found from {} to {}", start, end),
+        Some(path) => info!("path found from {} to {}: {:?}", start, end, path),
+        None => info!("no path found from {} to {}", start, end),
     }
 }
 
