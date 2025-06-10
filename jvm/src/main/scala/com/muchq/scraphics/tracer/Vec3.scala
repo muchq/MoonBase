ec556e9dc04e9cfa282bbd69ff12f5768c64b208
Andy Aylward <andy@eggsample.net>
2025-06-09 21:58:08 -0400

    resurrect scala support

--- jvm/src/main/scala/com/muchq/scraphics/tracer/Vec3.scala
+++ jvm/src/main/scala/com/muchq/scraphics/tracer/Vec3.scala
@@ -2,17 +2,17 @@ package com.muchq.scraphics
 package tracer
 
 case class Vec3(x: Double, y: Double, z: Double):
-  def +(o: Vec3): Vec3 =
+  infix def +(o: Vec3): Vec3 =
     Vec3(x + o.x, y + o.y, z + o.z)
 
-  def -(o: Vec3): Vec3 =
+  infix def -(o: Vec3): Vec3 =
     Vec3(x - o.x, y - o.y, z - o.z)
 
   def length(): Double =
     Math.sqrt(this dot this)
 
-  def *(d: Double): Vec3 =
+  infix def *(d: Double): Vec3 =
     Vec3(x * d, y * d, z * d)
 
-  def dot(o: Vec3): Double =
+  infix def dot(o: Vec3): Double =
     x * o.x + y * o.y + z * o.z
