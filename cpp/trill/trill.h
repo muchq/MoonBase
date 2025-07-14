fe1cf9fbc96bfe851a640b89cd0e69f3e64d39ae
Andy Aylward <andy@eggsample.net>
2025-07-14 10:15:09 -0400

    single header + createwindowandrenderer

--- cpp/trill/trill.h
+++ cpp/trill/trill.h
@@ -1,9 +1,7 @@
 #ifndef CPP_TRILL_TRILL_H
 #define CPP_TRILL_TRILL_H
 
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_video.h>
+#include <SDL3/SDL.h>
 
 #include <string>
 
