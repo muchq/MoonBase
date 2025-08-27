1beadf479932993004c5dc3a4ed73c7ce3cb7d57
Andy Aylward <andy@eggsample.net>
2025-08-26 22:30:23 -0400

    [portrait] b64 response struct (#832)
    * add base64 stuff
    
    * add cc_test
    
    * fmt
    
    * [portrait] b64 png + response struct

---
+++ cpp/portrait/base64.h
@@ -0,0 +1,28 @@
+#ifndef CPP_PORTRAIT_BASE64_H_
+#define CPP_PORTRAIT_BASE64_H_
+
+#include <string>
+#include <vector>
+
+namespace portrait {
+
+class Base64 {
+ public:
+  static std::string encode(const std::vector<uint8_t>& data);
+
+  static std::string encode(const uint8_t* data, size_t length);
+
+  static std::vector<uint8_t> decode(const std::string& encoded);
+
+  static size_t getEncodedSize(size_t input_size);
+
+  static size_t getDecodedSize(const std::string& encoded);
+};
+
+std::string pngToBase64(const std::vector<uint8_t>& png_data);
+
+std::vector<uint8_t> base64ToPng(const std::string& base64_data);
+
+}  // namespace portrait
+
+#endif  // CPP_PORTRAIT_BASE64_H_
