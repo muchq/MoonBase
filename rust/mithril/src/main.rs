21e2dd90632355fe1c22b534243b82bcc1ed4fac
Andy Aylward <andy@eggsample.net>
2025-09-20 22:27:54 -0400

    [oci] include runfiles

--- rust/mithril/src/main.rs
+++ rust/mithril/src/main.rs
@@ -69,8 +69,9 @@ async fn wordchain_post(
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let path = "rust/mithril/data/words.txt";
-    let word_graph = initialize_graph(path, Some("rust/mithril/data"));
+    let data_dir = "mithril.runfiles/_main/rust/mithril/data";
+    let path = format!("{}/words.txt", data_dir);
+    let word_graph = initialize_graph(&path, Some(data_dir));
 
     let shared_state = Arc::new(AppState { word_graph: word_graph });
 
