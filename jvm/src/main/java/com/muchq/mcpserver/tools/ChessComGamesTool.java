12add20ef09f140f06fee2e492a9c10bfe4a738e
Andy Aylward <andy@eggsample.net>
2026-01-11 16:52:53 -0500

    [mcpserver] add chess.com games tool

---
+++ jvm/src/main/java/com/muchq/mcpserver/tools/ChessComGamesTool.java
@@ -0,0 +1,57 @@
+package com.muchq.mcpserver.tools;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.muchq.chess_com_api.ChessClient;
+
+import java.io.IOException;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Map;
+
+public class ChessComGamesTool implements McpTool {
+
+    private final ChessClient chessClient;
+    private final ObjectMapper mapper;
+
+    public ChessComGamesTool(ChessClient chessClient, ObjectMapper mapper) {
+        this.chessClient = chessClient;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public String getName() {
+        return "chess_com_games";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Returns the requested user's chess.com games for the specified month and year. For example, username: hikaru, year: 2025, month: 01";
+    }
+
+    @Override
+    public Map<String, Object> getInputSchema() {
+        return Map.of(
+                "type", "object",
+                "properties", Map.of(
+                        "username", Map.of("type", "string", "description", "The player's chess.com username"),
+                        "year", Map.of("type", "string", "description", "The year the games were played (yyyy format)"),
+                        "month", Map.of("type", "string", "description", "The month the games were played (MM format)")
+                        ),
+                "required", List.of("message")
+        );
+    }
+
+    @Override
+    public String execute(Map<String, Object> arguments) {
+        String player = (String) arguments.get("message");
+        int year = ((Number) arguments.get("year")).intValue();
+        int month = ((Number) arguments.get("month")).intValue();
+        var gamesMaybe = chessClient.fetchGames(player, YearMonth.of(year, month));
+
+        try {
+            return mapper.writeValueAsString(gamesMaybe);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
