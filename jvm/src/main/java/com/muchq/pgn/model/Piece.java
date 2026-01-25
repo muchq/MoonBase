b7b80b7286618ea0b08287a56c28eb6eac833482
Andy Aylward <aaylward@gmail.com>
2026-01-24 23:00:21 -0500

    [pgn] stubs and tests

---
+++ jvm/src/main/java/com/muchq/pgn/model/Piece.java
@@ -0,0 +1,24 @@
+package com.muchq.pgn.model;
+
+public enum Piece {
+    KING('K'),
+    QUEEN('Q'),
+    ROOK('R'),
+    BISHOP('B'),
+    KNIGHT('N'),
+    PAWN('\0');
+
+    private final char symbol;
+
+    Piece(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char symbol() {
+        return symbol;
+    }
+
+    public static Piece fromSymbol(char c) {
+        throw new UnsupportedOperationException("TODO: implement");
+    }
+}
